a6f3e400783088c3b94fd2b9c75a701d1c03d206
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-06-17 11:45:23 +0900

    Update 0012_A_Point_in_a_Triangle.java
    http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=0012

--- 0012_A_Point_in_a_Triangle.java
+++ 0012_A_Point_in_a_Triangle.java
@@ -20,11 +20,7 @@ public class Main {
             double c1 = cross(x1, x2, y1, y2, xp, yp);
             double c2 = cross(x2, x3, y2, y3, xp, yp);
             double c3 = cross(x3, x1, y3, y1, xp, yp);
-            /*
-            double c1 = (x2-x1) * (yp-y2) - (y2-y1) * (xp-x2);
-            double c2 = (x3-x2) * (yp-y3) - (y3-y2) * (xp-x3);
-            double c3 = (x1-x3) * (yp-y1) - (y1-y3) * (xp-x1);
-            */
+
             if ((c1 > 0 && c2 > 0 && c3 > 0)||(c1 < 0 && c2 < 0 && c3 < 0)){
                 System.out.println("YES");
             }else{
