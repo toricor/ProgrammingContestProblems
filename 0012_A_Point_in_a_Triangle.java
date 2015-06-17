7815a907423453905c0c8e57119ab8e92ad97fcf
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-06-17 11:44:36 +0900

    Update 0012_A_Point_in_a_Triangle.java

--- 0012_A_Point_in_a_Triangle.java
+++ 0012_A_Point_in_a_Triangle.java
@@ -25,7 +25,7 @@ public class Main {
             double c2 = (x3-x2) * (yp-y3) - (y3-y2) * (xp-x3);
             double c3 = (x1-x3) * (yp-y1) - (y1-y3) * (xp-x1);
             */
-            if ((c1 > 0&& c2 > 0 && c3 > 0)||(c1 < 0 && c2 < 0 && c3 < 0)){
+            if ((c1 > 0 && c2 > 0 && c3 > 0)||(c1 < 0 && c2 < 0 && c3 < 0)){
                 System.out.println("YES");
             }else{
                 System.out.println("NO");
