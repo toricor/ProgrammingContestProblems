0e3c418f9a76d5aff234741e2a87f33b52d52f4b
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-08-19 14:30:10 +0900

    Create CodeThanksFestival2014A_A.scala

---
+++ AtCoder/CodeThanksFestival2014A_A.scala
@@ -0,0 +1,7 @@
+object Main{
+  def main(args:Array[String]) : Unit = {
+      val sc = new java.util.Scanner(System.in)
+      val A = sc.nextInt
+      val B = sc.nextInt
+      println(4 * A + 2 * B)
+}
