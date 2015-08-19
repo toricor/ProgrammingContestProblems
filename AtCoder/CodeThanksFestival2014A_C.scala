ab124e2ea3e8a7fe27c49eac674c7eb2a264cee5
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-08-20 08:38:25 +0900

    Update CodeThanksFestival2014A_C.scala

--- AtCoder/CodeThanksFestival2014A_C.scala
+++ AtCoder/CodeThanksFestival2014A_C.scala
@@ -4,6 +4,7 @@ object Main {
     val N, M = sc.nextInt
     val points = List.fill(N)(sc.nextInt)
     val corrects = List.fill(M)(sc.nextInt)
+    
     var total = 0
     for (num <- corrects)
       total += points(num - 1)
