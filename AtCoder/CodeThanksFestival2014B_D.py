d81ad5d095252f42020e06e281f5828df84d2a96
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-08-19 22:33:30 +0900

    Create CodeThanksFestival2014B_D.py

---
+++ AtCoder/CodeThanksFestival2014B_D.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+def fillxtimes(A, ashiList):
+    for i in xrange(1, len(ashiList)):
+        if i%A == 0:
+            ashiList[i] += 1
+            
+if __name__ == "__main__":
+    N, T = map(int, raw_input().split())
+    AList = []
+    ashiList = [0 for i in xrange(T + 1)]
+    for i in xrange(N):
+        A = int(raw_input())
+        AList.append(A)
+        fillxtimes(A, ashiList)
+    print max(ashiList)
