f82840f100dd7214f292f25e15708488b6670bf0
toricor <irotoridoritoriyaben@yahoo.co.jp>
2015-11-20 18:00:31 +0900

    Update GRL_1_A.py

--- AOJ/GRL_1_A.py
+++ AOJ/GRL_1_A.py
@@ -2,11 +2,11 @@
 from heapq import heappush, heappop
 
 class Node:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self): #, name):
+        #self.name = name
         self.edges_to = []
         self.edges_cost = []
-        self.done = False
+        #self.done = False
         self.cost = -1
 
 if __name__ == "__main__":
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     V, E, r = map(int, raw_input().split())
     nodes = []
     for i in xrange(V):         # V個のNodeを初期化
-        nodes.append(Node(i)) 
+        nodes.append(Node())#i)) 
     
     for i in xrange(E):
         s, t, d = map(int, raw_input().split())
@@ -24,12 +24,12 @@ if __name__ == "__main__":
     # dijkstra by priority queue(heapq)
     hq = []
     nodes[r].cost = 0           #始点rのコストは0
-    nodes[r].done = True  
+    #nodes[r].done = True  
     heappush(hq, nodes[r])
     
-    while len(hq)!= 0:          # rから各点への最短経路上の重みの和を求める。
+    while hq:          # rから各点への最短経路上の重みの和を求める。
         done_node = heappop(hq)
-        done_node.done = True
+        #done_node.done = True
         for to, cost in zip(done_node.edges_to, done_node.edges_cost):
             new_cost = done_node.cost + cost
             if nodes[to].cost < 0 or new_cost < nodes[to].cost:
