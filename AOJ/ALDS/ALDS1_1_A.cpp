f01079046d28f3939e8f8bdef0e720f58ee11438
toricor <irotoridoritoriyaben@yahoo.co.jp>
2016-05-23 16:56:37 +0900

    AC

---
+++ AOJ/ALDS/ALDS1_1_A.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+void printA(int A[], int n){
+    for (int k = 0; k < n; k++){
+        if (k != n - 1){
+            cout << A[k] << " ";
+        }else{
+            cout << A[n - 1] << endl;
+        }
+    }
+}
+int main(void){
+    int n;
+    cin >> n;
+    int A[n];
+    for (int i = 0; i < n; i++){
+        int item;
+        cin >> item;
+        A[i] = item;
+    }
+    // insertion sort
+    for (int i = 0; i < n; i++){
+        int key;
+        key = A[i];
+        int j = i - 1;
+        while (j >= 0 && A[j] > key){
+            A[j + 1] = A[j];
+            j--;
+        }
+        A[j + 1] = key;
+        printA(A, n);
+    }
+}
